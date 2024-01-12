e194fd09bf9512538110b37d5c1a96c19766c509
Matthew Bramich <matt_bramich@hotmail.com>
2024-01-12 12:56:57 +1100

    feat: init boilerplate

---
+++ lint-staged.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+const buildEslintCommand = (filenames) =>
+	`next lint --fix --file ${filenames
+		.map((f) => path.relative(process.cwd(), f))
+		.join(' --file ')}`;
+
+module.exports = {
+	'*.{js,jsx,ts,tsx}': ['prettier --write', buildEslintCommand],
+	'**/*.ts?(x)': () => 'npm run check-types',
+	'*.{json,yaml}': ['prettier --write']
+};
