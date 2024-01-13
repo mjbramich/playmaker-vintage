ac737b4088d135d117e93da09d7caa2e5b6d45e9
Matthew Bramich <matt_bramich@hotmail.com>
2024-01-13 15:24:19 +1100

    style: change auth layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 	return (
 		<ClerkProvider>
 			<html lang='en'>
-				<body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
+				<body className={cn('bg-background font-sans antialiased', fontSans.variable)}>
 					{children}
 				</body>
 			</html>
