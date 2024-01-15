465ec02e502db66a0a0e2c5b630a7efb7db05398
Matthew Bramich <matt_bramich@hotmail.com>
2024-01-15 17:54:10 +1100

    feat: create form component

--- src/components/modals/StoreModal.tsx
+++ src/components/modals/StoreModal.tsx
@@ -1,10 +1,39 @@
 'use-client';
 
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
 import useModalStore from '@/stores/modal';
 import Modal from '@/components/ui/modal';
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 
+const formSchema = z.object({
+	name: z.string().min(1, { message: 'Name is required' })
+});
 const StoreModal = () => {
 	const storeModal = useModalStore();
+
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			name: ''
+		}
+	});
+
+	// Handle form submission
+	const onSubmit = async (values: z.infer<typeof formSchema>) => {
+		console.log(values);
+	};
 
 	return (
 		<Modal
@@ -13,7 +42,31 @@ const StoreModal = () => {
 			isOpen={storeModal.isOpen}
 			onClose={storeModal.onClose}
 		>
-			Creat Store Form
+			<div className='space-y-4 py-2 pb-4'>
+				<Form {...form}>
+					<form onSubmit={form.handleSubmit(onSubmit)}>
+						<FormField
+							control={form.control}
+							name='name'
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Name</FormLabel>
+									<FormControl>
+										<Input placeholder='Name' {...field} />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<div className='flex items-center justify-end space-x-2 pt-6 '>
+							<Button variant='outline' onClick={storeModal.onClose}>
+								Cancel
+							</Button>
+							<Button type='submit'>Continue</Button>
+						</div>
+					</form>
+				</Form>
+			</div>
 		</Modal>
 	);
 };
