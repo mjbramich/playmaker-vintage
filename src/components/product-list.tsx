f6eb788eae58d468ebe67e0ded10a3e6fed843d1
Matthew Bramich <matt_bramich@hotmail.com>
2024-02-17 10:56:34 +1100

    style: adjust padding & responsiveness productlist

--- src/components/product-list.tsx
+++ src/components/product-list.tsx
@@ -15,14 +15,14 @@ interface Props {
 
 const ProductList = ({ data, title, link }: Props) => (
 	<section className='bg-white'>
-		<div className='py-16 sm:py-24 lg:mx-auto lg:max-w-7xl lg:px-8 lg:py-32'>
+		<div className='py-16 sm:py-24 lg:mx-auto lg:max-w-7xl lg:px-6 '>
 			{title && (
 				<div className='flex items-center justify-between px-4 sm:px-6 lg:px-0'>
-					<h2 className='text-2xl font-bold tracking-tight text-gray-900'>{title}</h2>
+					<h2 className='text-2xl font-bold uppercase tracking-tight text-gray-900'>{title}</h2>
 				</div>
 			)}
 
-			<ul className=' mt-6 grid grid-cols-2 gap-x-6 gap-y-10 px-4 sm:px-6 md:grid-cols-3 lg:grid-cols-4 lg:px-0 xl:gap-x-8'>
+			<ul className=' mt-6 grid grid-cols-2 gap-x-6 gap-y-10 px-4 sm:px-6 md:grid-cols-4 lg:grid-cols-4 lg:px-0 xl:gap-x-8'>
 				{data.map((product) => (
 					<ProductCard key={product.id} productData={product} />
 				))}
