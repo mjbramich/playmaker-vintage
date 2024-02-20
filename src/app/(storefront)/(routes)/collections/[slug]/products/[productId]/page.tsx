49eca5a447d9cd0645d1b37c5448c87b880f6384
Matthew Bramich <matt_bramich@hotmail.com>
2024-02-20 17:11:18 +1100

    feat: create individual product page

---
+++ src/app/(storefront)/(routes)/collections/[slug]/products/[productId]/page.tsx
@@ -0,0 +1,73 @@
+import prisma from '@/lib/prismadb';
+import { format } from 'date-fns';
+import { currencyFormatter } from '@/lib/utils';
+import { ProductWithImage } from '@/types';
+import ProductInfo from '@/components/product-info';
+import ProductGallery from '@/components/product-gallery';
+import ProductList from '@/components/product-list';
+
+export default async function ProductPage({ params }: { params: { productId: string } }) {
+	// get product
+	const product = await prisma.product.findUnique({
+		where: {
+			id: params.productId
+		},
+		include: {
+			images: true,
+			category: true
+		}
+	});
+
+	const relatedProducts = await prisma.product.findMany({
+		where: {
+			categoryId: product?.categoryId,
+			// don't include current product
+			id: {
+				not: product?.id
+			}
+		},
+		include: {
+			images: true,
+			category: true
+		}
+	});
+
+	if (!product) {
+		return null;
+	}
+
+	const formattedProduct: ProductWithImage = {
+		...product,
+		price: currencyFormatter.format(Number(product.price)),
+		categoryId: product.category.id,
+		category: product.category.name,
+		createdAt: format(new Date(product.createdAt), 'MMMM do, yyyy'),
+		updatedAt: format(new Date(product.updatedAt), 'MMMM do, yyyy')
+	};
+
+	const formattedProducts: ProductWithImage[] = relatedProducts.map((item) => ({
+		...item,
+		price: currencyFormatter.format(Number(item.price)), // Need to convert Decimal to number first then format
+		categoryId: item.categoryId,
+		category: item.category.name,
+		createdAt: format(item.createdAt, 'MMMM do, yyyy'),
+		updatedAt: format(item.updatedAt, 'MMMM do, yyyy')
+	}));
+
+	return (
+		<section className='bg-white'>
+			<div className='grid px-4 pt-8 sm:px-6 sm:pt-16 md:grid-cols-2 md:gap-x-8 lg:mx-auto lg:max-w-7xl'>
+				<ProductGallery productImages={formattedProduct.images} />
+				<ProductInfo product={formattedProduct} />
+			</div>
+			<ProductList
+				title='You May Also Like'
+				data={formattedProducts}
+				link={{
+					text: `Back to ${product.category.name}`,
+					href: `/collections/${product.category.name}`
+				}}
+			/>
+		</section>
+	);
+}
---
+++ src/components/product-info.tsx
@@ -0,0 +1,33 @@
+import { ProductWithImage } from '@/types';
+import { Button } from './ui/button';
+
+interface Props {
+	product: ProductWithImage;
+}
+
+const ProductInfo = ({ product }: Props) => (
+	<div className='mx-auto mt-12 w-full max-w-2xl md:mt-0'>
+		<h1 className='text-3xl font-bold capitalize tracking-tight text-gray-900'>{product.name}</h1>
+
+		<div className='space-y-6'>
+			<h2 className='sr-only'>Product information</h2>
+			<p className='text-3xl font-bold tracking-tight'>{product.price}</p>
+			<p className='text-xl font-semibold tracking-tight'>Size: {product.size}</p>
+		</div>
+
+		<form className='mt-6'>
+			<div className=''>
+				<Button className='w-full' size='lg'>
+					Add to cart
+				</Button>
+			</div>
+		</form>
+
+		<div className='mt-6'>
+			<h3 className='sr-only'>Description</h3>
+			<div className='space-y-6 text-base text-muted-foreground'>Description:</div>
+		</div>
+	</div>
+);
+
+export default ProductInfo;
