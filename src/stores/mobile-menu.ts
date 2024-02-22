119f6ff07470d93676b9d191e8d58b7b8b421ba2
Matthew Bramich <matt_bramich@hotmail.com>
2024-02-22 15:41:24 +1100

    refactor: update types in mobile menu store
    This commit improves the readability the types
    breaking the types down into seperate state and actions

--- src/stores/mobile-menu.ts
+++ src/stores/mobile-menu.ts
@@ -1,12 +1,15 @@
 import { create } from 'zustand';
 
-interface MobileMenuStore {
+interface State {
 	isOpen: boolean;
+}
+
+interface Actions {
 	toggleMenu: () => void;
 	closeMenu: () => void;
 }
 
-const useMobileMenuStore = create<MobileMenuStore>((set) => ({
+const useMobileMenuStore = create<State & Actions>()((set) => ({
 	isOpen: false, // default false
 	toggleMenu: () => set((state) => ({ isOpen: !state.isOpen })),
 	closeMenu: () => set({ isOpen: false })
