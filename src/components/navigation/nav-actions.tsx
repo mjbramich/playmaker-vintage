87133b845091e672a25ee9628ffe017b0cd1c818
Matthew Bramich <matt_bramich@hotmail.com>
2024-02-22 15:27:56 +1100

    refactor: added the number of items in cart to nav

--- src/components/navigation/nav-actions.tsx
+++ src/components/navigation/nav-actions.tsx
@@ -5,10 +5,13 @@ import { useRouter } from 'next/navigation';
 import { ShoppingCart } from 'lucide-react';
 import { UserButton, SignedIn, SignedOut } from '@clerk/nextjs';
 
+import useStore from '@/hooks/useStore';
+import useCartStore from '@/stores/cart';
 import { Button } from '@/components/ui/button';
 
 const NavActions = () => {
 	const router = useRouter();
+	const products = useStore(useCartStore, (state) => state.items);
 
 	return (
 		<div className='flex items-center space-x-4 lg:space-x-6'>
@@ -40,7 +43,7 @@ const NavActions = () => {
 				className='flex items-center p-0'
 			>
 				<ShoppingCart size={20} color='black' />
-				<span className='ml-2 text-sm font-medium'>0</span>
+				<span className='ml-2 text-sm font-medium'>{products?.length}</span>
 			</Button>
 		</div>
 	);
