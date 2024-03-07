46638855849486ce8e174566d3b49520d1546694
Matthew Bramich <matt_bramich@hotmail.com>
2024-03-07 16:59:44 +1100

    chore: remove redundant components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter as FontSans } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
-import ModalProvider from '@/providers/modalProviders';
 import ToastProvider from '@/providers/toastProvider';
 import '@css/globals.css';
 import { cn } from '@lib/utils';
@@ -31,7 +30,6 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 		<ClerkProvider>
 			<html lang='en'>
 				<body className={cn('bg-background font-sans antialiased', fontSans.variable)}>
-					<ModalProvider />
 					<ToastProvider />
 					<Navbar collections={collections} />
 					<PageContainer>{children}</PageContainer>
